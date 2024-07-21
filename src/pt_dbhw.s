d4551a46f36d25604a9cc8676041824c651b7124
kg68k <78926718+kg68k@users.noreply.github.com>
2024-07-22 01:53:32 +0900

    feat (pt_dbhw): improve output text

--- src/pt_dbhw.s
+++ src/pt_dbhw.s
@@ -1,7 +1,7 @@
 .title pt_dbhw - print text: double byte half width characters
 
 ;This file is part of Xperiment68k
-;Copyright (C) 2023 TcbnErik
+;Copyright (C) 2024 TcbnErik
 ;
 ;This program is free software: you can redistribute it and/or modify
 ;it under the terms of the GNU General Public License as published by
@@ -20,6 +20,8 @@
 .include macro.mac
 .include console.mac
 .include doscall.mac
+
+.include xputil.mac
 
 
 .cpu 68000
@@ -60,23 +62,20 @@ CreateCharTable:
   PUSH d5-d7/a0-a1
   move.b d0,d7
 
-  lea (Header80,pc),a1
-  cmpi.b #$f0,d7
-  bcs @f
-    lea (HeaderF0,pc),a1
-    moveq #$f,d0
-    and.b d7,d0
-    lsl #2,d0
-    adda.l d0,a1
-  @@:
-  STRCPY a1,a0,-1
-  lea (HeaderCol,pc),a1
+  bsr ToHexString2  ;上位バイト
+  lea (HeaderCol+2,pc),a1
   STRCPY a1,a0,-1
 
-  lea (HeaderRow,pc),a1
   moveq #SPACE,d5
   moveq #$00,d6
   1:
+    lea (HeaderRow,pc),a1
+    move.b (a1)+,(a0)+
+    move.b (a1)+,(a0)+
+    move.b d6,d0
+    bsr ToHexString2  ;下位バイト(の上位ニブル)
+    addq.l #1,a1      ;下位ニブルを'_'で上書きする
+    subq.l #1,a0
     STRCPY a1,a0,-1
     2:
       move.b d5,(a0)+
@@ -87,16 +86,20 @@ CreateCharTable:
     moveq #$f,d0
     and.b d6,d0
     bne 2b
-    move.b #LF,(-1,a0)
+    subq.l #1,a0  ;最後の空白を取り消す
+    move.b #CR,(a0)+
+    move.b #LF,(a0)+
   tst.b d6
   bne 1b
 
-  move.b #LF,(a0)+
   clr.b (a0)
   move.l a0,d0
   POP d5-d7/a0-a1
   sub.l a0,d0  ;length
   rts
+
+
+  DEFINE_TOHEXSTRING2 ToHexString2
 
 
 .bss
@@ -107,20 +110,11 @@ Buffer: .ds.b 2048
 
 .data
 
-Header80:
-  .dc.b '80',0
-HeaderF0:
-  .irpc x,012345
-    .dc.b '&x0',0,0
-  .endm
-
 HeaderCol:
-  .dc.b   ' | _0 _1 _2 _3 _4 _5 _6 _7 _8 _9 _a _b _c _d _e _f',LF
-  .dc.b '---+------------------------------------------------',LF,0
+  .dc.b 'XX__ | _0 _1 _2 _3 _4 _5 _6 _7 _8 _9 _a _b _c _d _e _f',CR,LF
+  .dc.b '-----+------------------------------------------------',CR,LF,0
 HeaderRow:
-  .irpc x,0123456789abcdef
-    .dc.b '&x_ | ',0
-  .endm
+  .dc.b '  Y_ | ',0
 
 
 .end ProgramStart
