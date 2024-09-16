6454d734f1bf1201f53d984bb2401c2f2d19a5ba
kg68k <78926718+kg68k@users.noreply.github.com>
2024-09-17 07:17:59 +0900

    feat: add dos_mkdir

---
+++ src/dos_mkdir.s
@@ -0,0 +1,45 @@
+.title dos_mkdir - DOS _MKDIR
+
+;This file is part of Xperiment68k
+;Copyright (C) 2024 TcbnErik
+;
+;This program is free software: you can redistribute it and/or modify
+;it under the terms of the GNU General Public License as published by
+;the Free Software Foundation, either version 3 of the License, or
+;(at your option) any later version.
+;
+;This program is distributed in the hope that it will be useful,
+;but WITHOUT ANY WARRANTY; without even the implied warranty of
+;MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;GNU General Public License for more details.
+;
+;You should have received a copy of the GNU General Public License
+;along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+.include macro.mac
+.include console.mac
+.include doscall.mac
+
+.include xputil.mac
+
+
+.cpu 68000
+.text
+
+Start:
+  addq.l #1,a2
+  SKIP_SPACE a2  ;空文字列も受け付ける(挙動確認用)
+
+  pea (a2)
+  DOS _MKDIR
+  addq.l #4,sp
+  bsr PrintD0$4_4
+  DOS_PRINT_CRLF
+
+  DOS _EXIT 
+
+
+  DEFINE_PRINTD0$4_4 PrintD0$4_4
+
+
+.end
