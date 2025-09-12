fc67073bbf048e774ddb1b9b50f5bb515f0241a8
kg68k <78926718+kg68k@users.noreply.github.com>
2025-09-13 06:50:30 +0900

    feat: add dos_chdir

---
+++ src/dos/dos_chdir.s
@@ -0,0 +1,48 @@
+.title dos_chdir - DOS _CHDIR
+
+;This file is part of Xperiment68k
+;Copyright (C) 2025 TcbnErik
+;
+;This program is free software: you can redistribute it and/or modify
+;it under the terms of the GNU General Public License as published by
+;the Free Software Foundation, either version 3 of the License, or
+;(at your option) any later version.
+;
+;This program is distributed in the hope that it will be useful,
+;but WITHOUT ANY WARRANTY; without even the implied warranty of
+;MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;GNU General Public License for more details.
+;
+;You should have received a copy of the GNU General Public License
+;along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+.include console.mac
+.include doscall.mac
+
+.include xputil.mac
+
+
+.cpu 68000
+.text
+
+ProgramStart:
+  addq.l #1,a2
+  SKIP_SPACE a2
+  pea (a2)
+  DOS _CHDIR
+  addq.l #4,sp
+
+  bsr Print$4_4
+  DOS_PRINT (CrLf,pc)
+  DOS _EXIT
+
+
+  DEFINE_PRINT$4_4 Print$4_4
+
+
+.data
+
+CrLf: .dc.b CR,LF,0
+
+
+.end ProgramStart
