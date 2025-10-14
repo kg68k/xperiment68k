2765c5437a1a9b17530fe0473c2f54ec907e7762
kg68k <78926718+kg68k@users.noreply.github.com>
2025-10-15 07:53:19 +0900

    feat (m_assign): support OPMDRV3.X features

--- src/opmdrv/m_assign.s
+++ src/opmdrv/m_assign.s
@@ -16,7 +16,8 @@
 ;You should have received a copy of the GNU General Public License
 ;along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-.include opmdrv.mac
+.include macro.mac
+.include opmdrvdef.mac
 
 .include xputil.mac
 
@@ -27,31 +28,86 @@
 ProgramStart:
   lea (1,a2),a0
   SKIP_SPACE a0
-  beq PrintUsage
-
-  bsr ParseIntWord
-  move d0,d2  ;チャンネル番号
-  swap d2
-  SKIP_SPACE a0
-  beq PrintUsage
-
-  bsr ParseIntWord
-  move d0,d2  ;トラック番号
-
+  bne @f
+    bsr PrintAllChannels  ;引数省略時は全チャンネルの割り当てを表示(OPMDRV3.X専用)
+    bra 9f
+  @@:
+    bsr ParseIntWord
+    move d0,d2  ;チャンネル番号
+    swap d2
+    move #O3_ASSIGN_INQUIRY,d2  ;トラック番号省略時は割り当て取得(OPMDRV3.X専用)
+    SKIP_SPACE a0
+    beq @f
+      bsr ParseIntWord
+      move d0,d2  ;トラック番号
+  @@:
   OPM _M_ASSIGN
   bsr Print$4_4
   DOS_PRINT_CRLF
-
+9:
   DOS _EXIT
 
 
-PrintUsage:
-  PRINT_1LINE_USAGE 'usage: m_assign <channel_no> <track_no>'
-  DOS _EXIT
+PrintAllChannels:
+  PUSH d6-d7
+  lea (Buffer,pc),a0
+  lea (strHeader,pc),a1
+  STRCPY a1,a0,-1
+
+  moveq #O3_CHANNEL_COUNT-1,d7
+  moveq #O3_CHANNEL_MIN,d6
+  1:
+    move.l d6,d0
+    moveq #2,d1
+    bsr ToDecStringWidth
+    lea (strColon,pc),a1
+    STRCPY a1,a0,-1
+
+    move d6,d2  ;チャンネル番号
+    swap d2
+    move #O3_ASSIGN_INQUIRY,d2
+    OPM _M_ASSIGN
+
+    cmpi.l #O3_TRACK_MIN,d0
+    bcs @f
+    cmpi.l #O3_TRACK_MAX,d0
+    bhi @f
+      bsr ToDecString
+      bra 8f
+    @@:
+      bsr ToHexString$4_4  ;エラーコードは16進数で表示する
+    8:
+    lea (strCrLf,pc),a1
+    STRCPY a1,a0,-1
+
+    addq #1,d6
+  dbra d7,1b
+
+  clr.b (a0)
+  DOS_PRINT (Buffer,pc)
+  POP d6-d7
+  rts
 
 
-  DEFINE_PARSEINTWORD ParseIntWord
+  DEFINE_TODECSTRING ToDecString
+  DEFINE_TODECSTRINGWIDTH ToDecStringWidth
+  DEFINE_TOHEXSTRING$4_4 ToHexString$4_4
   DEFINE_PRINT$4_4 Print$4_4
+  DEFINE_PARSEINTWORD ParseIntWord
+
+
+.data
+
+strHeader: .dc.b 'Ch: Track',CR,LF,0
+strColon: .dc.b ': ',0
+
+strCrLf: .dc.b CR,LF,0
+
+
+.bss
+.even
+
+Buffer: .ds.b 1024
 
 
 .end ProgramStart
